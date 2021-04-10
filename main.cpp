b2580e1aac1e7e1d086843248c0b3ce889c596a6
Lawrence Jones <git@lwjon.es>
2021-04-09 17:54:18 -0700

    Setup main; project file

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/**
+ * CIS 22B
+ * LAB 1 - This program scrapes book title, author, and genre information from
+ *         external files and prints the title of each book in alphabetical
+ *         order.
+ * Lab Group # 21
+ * Partner Names: Adel Burieva, Lawrence Jones
+ * @date: 2021-04-09
+ */
+
+int main() {
+    return 0;
+}
